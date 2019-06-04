582e5e8013ba0c2c20276990fee0ba58e4b09077
Jacob <Specter@Jacobs-MacBook-Pro-4.local>
2019-06-04 14:41:48 -0500

    pushey push

--- server/server.js
+++ server/server.js
@@ -1,15 +1,5 @@
 const express = require('express');
 const bodyparser = require('body-parser');
-<<<<<<< HEAD
-const port = 3005
-app = express();
-app.use(bodyparser.json());
-app.use(express.static);
-
-app.listen(port, ()=>{console.log(`proxy server listening on port ${port}`)});
-
-
-=======
 const port = 3006
 require('dotenv').config();
 app = express();
@@ -18,4 +8,3 @@ app.use(express.static('client/dist'));
 app.listen(port, ()=>{
     console.log(`proxy server listening on port ${port}`)
 });
->>>>>>> dev
